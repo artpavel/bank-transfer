5126466ee12a226af049da43c02b1dc27622e569
Pavel <artpavel@ukr.net>
2022-04-08 17:16:08 +0300

    add date in the balance

--- js/script.js
+++ js/script.js
@@ -6,6 +6,18 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -13,6 +25,18 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -20,6 +44,18 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account4 = {
@@ -27,6 +63,15 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  movementsDates: [
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -58,24 +103,37 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 // print on screen our data-account
-const displayMovements = (movements, sort = false) => {
+const displayMovements = (acc, sort = false) => {
   // clear our old list
   containerMovements.innerHTML = '';
 
   // for sort our date
-  const movSort = sort ? [...movements].sort((a, b) => a - b) : movements;
+  const movSort = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
   // create new list
   movSort.forEach((mov, i) => {
     // check is or not deposit
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
+    // for show date
+    const date = new Date(acc.movementsDates[i]);
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    const hour = `${date.getHours()}`.padStart(2, 0);
+    const min = `${date.getMinutes()}`.padStart(2, 0);
+
+    const displayDate = `${day}/${month}/${year}, ${hour}:${min}`;
+
     const html = `
     <div class="movements__row">
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">${mov} €</div>
+      <div class="movements__date">${displayDate}</div>
+      <div class="movements__value">${mov.toFixed(2)} €</div>
     </div>
     `;
 
@@ -87,7 +145,7 @@ const displayMovements = (movements, sort = false) => {
 const calcDisplayBalance = account => {
   const balance = account.movements.reduce((acc, sum) => acc + sum, 0);
   account.balance = balance;
-  labelBalance.textContent = `${balance} €`;
+  labelBalance.textContent = `${balance.toFixed(2)} €`;
 };
 
 // all the income and spending
@@ -109,8 +167,8 @@ const calcDisplaySummary = account => {
     })
     .reduce((acc, mov) => acc + mov, 0);
 
-  labelSumIn.textContent = `${incomes} €`;
-  labelSumOut.textContent = `${Math.abs(out)} €`;
+  labelSumIn.textContent = `${incomes.toFixed(2)} €`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)} €`;
   labelSumInterest.textContent = `${interest.toFixed(2)} €`;
 };
 
@@ -130,7 +188,7 @@ createUsernames(accounts);
 // for display user UI
 const updateUI = account => {
   // display movements
-  displayMovements(account.movements);
+  displayMovements(account);
   // display balance
   calcDisplayBalance(account);
   // display summary
@@ -165,6 +223,16 @@ btnLogin.addEventListener('click', e => {
     labelWelcome.textContent = 'Log in to get started';
     alert('Not the correct user or password');
   }
+
+  // show date in the <current balance>
+  const now = new Date();
+  const day = `${now.getDate()}`.padStart(2, 0);
+  const month = `${now.getMonth() + 1}`.padStart(2, 0);
+  const year = now.getFullYear();
+  const hour = `${now.getHours()}`.padStart(2, 0);
+  const min = `${now.getMinutes()}`.padStart(2, 0);
+
+  labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 });
 
 // transfers money between users
@@ -184,6 +252,11 @@ btnTransfer.addEventListener('click', e => {
   ) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
+
+    // add transfer date
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
     inputTransferAmount.value = inputTransferTo.value = '';
 
     // update user UI
@@ -197,7 +270,7 @@ btnTransfer.addEventListener('click', e => {
 btnLoan.addEventListener('click', e => {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
   const isCanGiveCredit = currentAccount.movements.some(
     mov => mov >= amount * 0.1
   );
@@ -205,6 +278,8 @@ btnLoan.addEventListener('click', e => {
   if (amount > 0 && isCanGiveCredit) {
     // add movements
     currentAccount.movements.push(amount);
+    // add loan date
+    currentAccount.movementsDates.push(new Date().toISOString());
     // update user UI
     updateUI(currentAccount);
   } else {
@@ -242,7 +317,7 @@ btnClose.addEventListener('click', e => {
 let isSorted = false;
 btnSort.addEventListener('click', e => {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !isSorted);
+  displayMovements(currentAccount, !isSorted);
   // it helps to switch
   isSorted = !isSorted;
 });
